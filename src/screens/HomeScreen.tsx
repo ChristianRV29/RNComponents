9078a196899508b6714dc6cca334ca81ca9c7bc7
ChristianRV29 <kmylorv@gmail.com>
2022-03-04 17:06:55 -0500

    Flatlist modified with basic styles

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,63 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import Icon from 'react-native-vector-icons/Ionicons';
+import { styles } from '~src/theme/appTheme';
+
+interface ItemProps {
+  name: string;
+  icon: string;
+  component: string;
+}
+
+const menuItems: ItemProps[] = [
+  {
+    name: 'Animation 1',
+    icon: 'cube-outline',
+    component: 'Animation01Screen',
+  },
+  {
+    name: 'Animation 2',
+    icon: 'album-outline',
+    component: 'Animation02Screen',
+  },
+];
 
 const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
+
+  const renderMenuItem = (item: ItemProps): JSX.Element => {
+    return (
+      <View>
+        <Text
+          style={styles.listMenuLabel}
+        >{`${item.name} - ${item.icon}`}</Text>
+      </View>
+    );
+  };
+
+  const renderListHeader = (): JSX.Element => {
+    return (
+      <View style={{ marginTop: top + 15, marginBottom: 15 }}>
+        <Text style={styles.listTitle}>Menu options</Text>
+      </View>
+    );
+  };
+
+  const renderSeparator = () => {
+    return (
+      <View style={{ borderBottomWidth: 1, opacity: 0.4, marginVertical: 8 }} />
+    );
+  };
+
   return (
-    <View>
-      <Text>Home screen</Text>
-      <Icon name={'star-outline'} size={50} color={'green'} />
-      {/**<FontAwesomeIcon icon="fa-solid fa-stars" /> */}
+    <View style={styles.home}>
+      <FlatList
+        data={menuItems}
+        renderItem={({ item }) => renderMenuItem(item)}
+        ListHeaderComponent={renderListHeader}
+        ItemSeparatorComponent={renderSeparator}
+      />
     </View>
   );
 };
---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  home: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
+  listTitle: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  listMenuLabel: {
+    color: 'black',
+  },
+});
