502bc1198708557aeeb13f7045ddb92878c760d4
ChristianRV29 <kmylorv@gmail.com>
2022-03-04 17:37:39 -0500

    FlatList item extracted to a reusable component

---
+++ src/components/FlatList/MenuItem.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import { ItemProps } from '~src/@types/interfaces';
+
+interface MenuItemProps {
+  item: ItemProps;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ item }) => {
+  const { name, icon } = item;
+  return (
+    <View style={styles.container}>
+      <View style={styles.subContainer}>
+        <Icon name={icon} color={'gray'} size={23} />
+        <Text style={styles.itemLabel}>{name}</Text>
+      </View>
+      <View>
+        <Icon
+          name={'arrow-forward-circle-outline'}
+          color={'gray'}
+          size={23}
+          style={styles.iconArrow}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  subContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  itemLabel: {
+    marginHorizontal: 5,
+    fontSize: 19,
+    color: 'gray',
+  },
+  iconArrow: {
+    right: 0,
+  },
+});
+
+export default MenuItem;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,39 +2,25 @@ import React from 'react';
 import { View, FlatList, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+import { ItemProps } from '~src/@types/interfaces';
+import MenuItem from '~src/components/FlatList/MenuItem';
 import { styles } from '~src/theme/appTheme';
-
-interface ItemProps {
-  name: string;
-  icon: string;
-  component: string;
-}
 
 const menuItems: ItemProps[] = [
   {
     name: 'Animation 1',
     icon: 'cube-outline',
-    component: 'Animation01Screen',
+    screenName: 'Animation01Screen',
   },
   {
     name: 'Animation 2',
-    icon: 'album-outline',
-    component: 'Animation02Screen',
+    icon: 'albums-outline',
+    screenName: 'Animation02Screen',
   },
 ];
 
 const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
-
-  const renderMenuItem = (item: ItemProps): JSX.Element => {
-    return (
-      <View>
-        <Text
-          style={styles.listMenuLabel}
-        >{`${item.name} - ${item.icon}`}</Text>
-      </View>
-    );
-  };
 
   const renderListHeader = (): JSX.Element => {
     return (
@@ -54,7 +40,8 @@ const HomeScreen = () => {
     <View style={styles.home}>
       <FlatList
         data={menuItems}
-        renderItem={({ item }) => renderMenuItem(item)}
+        renderItem={({ item }) => <MenuItem item={item} />}
+        keyExtractor={item => item.name}
         ListHeaderComponent={renderListHeader}
         ItemSeparatorComponent={renderSeparator}
       />
