2c5b5f054aa1802e57f3cb032bd94609ab4a756d
ChristianRV29 <kmylorv@gmail.com>
2022-03-21 11:13:15 -0500

    TextInput component implemented

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 
 import Header from '~src/components/Header/Header';
-import { globalStyles } from '../theme/appTheme';
+import { globalStyles } from '~src/theme/appTheme';
 
-const TextInput = () => {
+const TextInputScreen = () => {
   return (
     <View style={globalStyles.container}>
-      <Header title="Text input" />
+      <Header title="TextInputs" />
+      <TextInput style={styles.textInput} />
     </View>
   );
 };
 
-export default TextInput;
+const styles = StyleSheet.create({
+  textInput: {
+    borderWidth: 1,
+    height: 50,
+    opacity: 0.8,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    borderColor: 'rgba(0, 0, 0, 0.8)',
+    color: 'gray',
+  },
+});
+
+export default TextInputScreen;
