06b8676b4d3878b38207d50cd8c98860694e2f0d
ChristianRV29 <kmylorv@gmail.com>
2022-05-19 09:33:34 -0500

    Implementation of useReducer for consume useContext

--- src/components/FlatList/MenuItem.tsx
+++ src/components/FlatList/MenuItem.tsx
@@ -1,11 +1,12 @@
-import { useNavigation, useTheme } from '@react-navigation/native';
-import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import React, { useContext } from 'react';
 
 import { View, Text, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { ItemProps } from '~src/@types/interfaces';
+import { ThemeContext } from '~src/context/theme/themeContext';
 
 interface MenuItemProps {
   item: ItemProps;
@@ -13,7 +14,8 @@ interface MenuItemProps {
 
 const MenuItem: React.FC<MenuItemProps> = ({ item }) => {
   const { navigate } = useNavigation();
-  const { colors } = useTheme();
+
+  const { theme } = useContext(ThemeContext);
 
   const { name, icon, screenName } = item;
   return (
@@ -22,13 +24,15 @@ const MenuItem: React.FC<MenuItemProps> = ({ item }) => {
       onPress={() => navigate(screenName as any)}
     >
       <View style={styles.subContainer}>
-        <Icon name={icon} color={'#5856D6'} size={23} />
-        <Text style={{ ...styles.itemLabel, color: colors.text }}>{name}</Text>
+        <Icon name={icon} color={theme.colors.primary} size={23} />
+        <Text style={{ ...styles.itemLabel, color: theme.colors.text }}>
+          {name}
+        </Text>
       </View>
       <View>
         <Icon
           name={'arrow-forward-circle-outline'}
-          color={'#5856D6'}
+          color={theme.colors.primary}
           size={23}
           style={styles.iconArrow}
         />
